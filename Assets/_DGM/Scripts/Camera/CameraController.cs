3975d89411969a9f649a201c7caee5cda2bd58d8
teeyou <teeyou1127@gmail.com>
2026-04-24 00:06:59 +0900

    [feat] 시작의마을 맵 제작

--- Assets/_DGM/Scripts/Camera/CameraController.cs
+++ Assets/_DGM/Scripts/Camera/CameraController.cs
@@ -10,28 +10,18 @@ using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private float _sensitivity;
     [SerializeField] private CinemachineVirtualCamera _vcam;
 
-    [SerializeField] private float _minPitch;
-    [SerializeField] private float _maxPitch;
-
-    [SerializeField] private float zoomSpeed;
-    [SerializeField] private float minZoom;
-    [SerializeField] private float maxZoom;
-
-    private float _pitch, _yaw;
-    private bool _isRightClicked = false;
     private Transform _target;
 
     private InputManager _inputManager;
 
     private CancellationTokenSource _cts;
 
-    private CinemachineTransposer _transposer;
+    //private CinemachineTransposer _transposer;
     private void Awake()
     {
-        _transposer = _vcam.GetCinemachineComponent<CinemachineTransposer>();
+        //_transposer = _vcam.GetCinemachineComponent<CinemachineTransposer>();
     }
 
     private void OnEnable()
@@ -39,8 +29,6 @@ public class CameraController : MonoBehaviour
         if (InputManager.Instance != null)
         {
             _inputManager = InputManager.Instance;
-            _inputManager.OnZoom += HandleZoom;
-            _inputManager.OnMouseRightClick += OnMouseRightClicked;
         }
 
         _cts = new CancellationTokenSource();
@@ -77,38 +65,5 @@ public class CameraController : MonoBehaviour
             _cts.Dispose();
             _cts = null;
         }
-    }
-
-    void LateUpdate()
-    {
-        //if (_target == null)
-        //    return;
-
-        //if (_isRightClicked)
-        //{
-        //    Vector2 delta = Mouse.current.delta.ReadValue();
-        //    _yaw += delta.x * _sensitivity * Time.deltaTime;
-        //    _pitch -= delta.y * _sensitivity * Time.deltaTime;
-        //    _pitch = Mathf.Clamp(_pitch, _minPitch, _maxPitch);
-        //}
-
-
-        //transform.LookAt(_target);
-    }
-
-    private void OnMouseRightClicked(bool clicked)
-    {
-        _isRightClicked = clicked;
-    }
-
-    private void HandleZoom(float value)
-    {
-        Vector3 offset = _transposer.m_FollowOffset;
-        float scale = 1f - value * zoomSpeed;
-        scale = Mathf.Clamp(scale, minZoom, maxZoom);
-
-        offset *= scale;
-
-        _transposer.m_FollowOffset = offset;
     }
 }
